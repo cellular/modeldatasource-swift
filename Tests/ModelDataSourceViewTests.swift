87c76d930e674f552706f2cab9180e495949b0ee
Michael Haß <michael.s.hass@googlemail.com>
2018-11-30 10:13:00 +0100

    Add UICollectionView + ModelDataSourceView tests

---
+++ Tests/ModelDataSourceViewTests.swift
@@ -0,0 +1,50 @@
+//
+//  ModelDataSourceViewTests.swift
+//  ModelDataSourceTests
+//
+//  Created by Michael Hass on 29.10.18.
+//  Copyright © 2018 CELLULAR GmbH. All rights reserved.
+//
+
+import XCTest
+
+class ModelDataSourceViewTests: XCTestCase {
+
+    // MARK: UITableView + ModelDataSourceView
+
+    func testTableViewRegisterCell() {
+        let tableView = UITableView()
+        tableView.register(TestTableViewCell.self)
+        let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: TestTableViewCell.reuseIdentifier)
+        XCTAssertTrue(dequeuedCell is TestTableViewCell)
+    }
+
+    func testTableViewRegisterDecorative() {
+        let tableView = UITableView()
+        tableView.register(TestTableViewDecorativeView.self)
+        let dequeuedView = tableView.dequeueReusableHeaderFooterView(withIdentifier: TestTableViewDecorativeView.reuseIdentifier)
+        XCTAssertTrue(dequeuedView is TestTableViewDecorativeView)
+    }
+
+    func testCollectionViewRegisterCell() {
+        let collectionView = UICollectionView.init(frame: .zero, collectionViewLayout: .init())
+        collectionView.register(TestCollectionViewCell.self)
+        collectionView.dequeueReusableCell(withReuseIdentifier: TestCollectionViewCell.reuseIdentifier, for: IndexPath())
+        XCTAssert(true, "Collection view dequeued cell successfully.")
+    }
+
+    func testCollectionViewRegisterDecorative() {
+        let collectionView = UICollectionView.init(frame: .zero, collectionViewLayout: .init())
+        collectionView.register(TestCollectionViewDecorativeView.self, as: .footer)
+        collectionView.register(TestCollectionViewDecorativeView.self, as: .header)
+        collectionView.dequeueReusableSupplementaryView(ofKind: String(describing: UICollectionView.DecorativeKind.footer),
+                                                        withReuseIdentifier: TestCollectionViewDecorativeView.reuseIdentifier,
+                                                        for: IndexPath())
+        collectionView.dequeueReusableSupplementaryView(ofKind: String(describing: UICollectionView.DecorativeKind.header),
+                                                        withReuseIdentifier: TestCollectionViewDecorativeView.reuseIdentifier,
+                                                        for: IndexPath())
+        XCTAssert(true, "Collection view dequeued header / footer successfully")
+    }
+}
+
+
